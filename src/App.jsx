1c205e4c9d12fb24ea7ad6ed0035c761cd0af585
GalibIbr <galibiavi4@gmail.com>
2023-09-14 16:52:46 +0300

    testm

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { Button } from '@mui/material'
-import './App.css'
-import Header from './components/header/Header'
-import Main from './components/main/Main'
+import { Button } from "@mui/material";
+import "./App.css";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <div></div>
       <Main></Main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
